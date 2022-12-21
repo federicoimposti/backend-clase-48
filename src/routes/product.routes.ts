71c379292c44949272e3b82964edcc33df30094d
Federico Imposti <federico.imposti@gmail.com>
2022-12-21 18:57:11 -0300

    fix routes id

--- src/routes/product.routes.ts
+++ src/routes/product.routes.ts
@@ -9,7 +9,7 @@ import {
 
 export const router = new Router()
   .get("/api/products", findAll)
-  .get("/api/products/:userId", findProduct)
+  .get("/api/products/:productId", findProduct)
   .post("/api/products", createProduct)
-  .put("/api/products/:userId", updateProduct)
-  .delete("/api/products/:userId", deleteProduct);
+  .put("/api/products/:productId", updateProduct)
+  .delete("/api/products/:productId", deleteProduct);
